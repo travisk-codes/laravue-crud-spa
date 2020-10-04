73848c378dccd709c00b964ecca5d4bd0591bfdc
Travis Kohlbeck <me+git@travisk.info>
2020-10-03 20:54:45 -0400

    Clean up TitleController

--- app/Http/Controllers/TitleController.php
+++ app/Http/Controllers/TitleController.php
@@ -3,63 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Title;
-use Illuminate\Http\Request;
 use App\Http\Requests\TitleRequest;
 
 class TitleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         return Title::paginate(10);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(TitleRequest $request)
     {
         $title = Title::create($request->validated());
         return response()->json($title);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Title  $title
-     * @return \Illuminate\Http\Response
-     */
     public function show(Title $title)
     {
         return response()->json($title);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Title  $title
-     * @return \Illuminate\Http\Response
-     */
     public function update(TitleRequest $request, Title $title)
     {
         $title->update($request->validated());
         return response()->json($title);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Title  $title
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Title $title)
     {
         $title->delete();
